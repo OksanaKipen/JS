cc05aee634261a2ce4187e86662eb257bd4b259f
oksanakipen <kipa.com88@gmail.com>
2023-05-01 23:35:03 +0300

    add Lesson_1

---
+++ Lesson_1/Lesson.js
@@ -0,0 +1,50 @@
+let hello = `hello`
+console.log(hello);
+let owu = `owu`
+console.log(owu);
+let com = `com`
+console.log(com);
+let ua = `ua`
+console.log(ua);
+let e = 1
+console.log(e);
+let f = 10
+console.log(f);
+let g = -999
+console.log(g);
+let h = 123
+console.log(h);
+let i = 3.14
+console.log(i);
+let j = 2.7
+console.log(j);
+let k = 16
+console.log(k);
+let check = true
+console.log(check);
+let check2 = false
+console.log(check2);
+
+
+let firstName = `Oksana`
+// console.log(firstName);
+let middleName = `Volodymyrivna`
+// console.log(middleName);
+let lastName = `Kipen`
+// console.log(lastName);
+let person = firstName +' ' +middleName +' ' +lastName;
+console.log(person);
+
+let a = 100
+console.log(typeof a);
+let b = '100'
+console.log(typeof b);
+let c = true
+console.log(typeof c);
+
+let userName = prompt('Enter your firstname:')
+console.log(userName);
+let userName1 = prompt('Enter your middle name:')
+console.log(userName1);
+let userName3 = prompt('Enter your lastname:')
+console.log(userName3);
